8acb1e830a783aff6748a0bac3e9fec318dc4f91
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-05-20 21:33:38 +0200

    adding testcases

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -22,6 +22,22 @@ public class UserTest {
 		User u = new User("Melly","mgoebel@ain.at");
 		assertEquals("mgoebel@ain.at",u.geteMail());		
 	}
+	@Test
+	public void testUserStringStringRight2() {
+		User u = new User("Melly","melanie.goebel@ain.at");
+		assertEquals("melanie.goebel@ain.at",u.geteMail());		
+	}
+	@Test
+	public void testUserStringStringRight3() {
+		User u = new User("Melly","melanie.goebel@ain.ac.at");
+		assertEquals("melanie.goebel@ain.ac.at",u.geteMail());		
+	}
+	
+	@Test
+	public void testUserStringStringRight5() {
+		User u = new User("User1","abcds");
+		assertEquals("no@email.com",u.geteMail());		
+	}
 
 	@Test
 	public void testGetUsername() {
