d7c18c6ef04b1a57918b5223c2f5af332c430976
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-05-20 13:58:28 +0200

    initial testcases

---
+++ src/test/UserTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
