5716a051f119413d0f8357cf7ddb0e75897cfb2f
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-05-21 08:39:36 +0200

    100% coverage done

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.*;
 import model.User;
 
 import org.junit.Test;
-
+/**
+ * Junit-Testcases for User
+ * @author Melanie Göbel
+ * @version 2014-05-21
+ */
 public class UserTest {
 
 	@Test
@@ -19,6 +23,18 @@ public class UserTest {
 	}
 
 	@Test
+	public void testUserStringNull() {
+		User u = new User(null);
+		assertEquals("User",u.getUsername());
+	}
+
+	@Test
+	public void testUserStringEmpty() {
+		User u = new User("");
+		assertEquals("User",u.getUsername());
+	}
+
+	@Test
 	public void testUserStringStringRight1() {
 		User u = new User("Melly","mgoebel@ain.at");
 		assertEquals("mgoebel@ain.at",u.geteMail());		
@@ -28,12 +44,13 @@ public class UserTest {
 		User u = new User("Melly","melanie.goebel@ain.at");
 		assertEquals("melanie.goebel@ain.at",u.geteMail());		
 	}
+
 	@Test
 	public void testUserStringStringRight3() {
 		User u = new User("Melly","melanie.goebel@ain.ac.at");
 		assertEquals("melanie.goebel@ain.ac.at",u.geteMail());		
 	}
-	
+
 	@Test
 	public void testUserStringStringRight5() {
 		User u = new User("User1","abcds");
@@ -41,9 +58,45 @@ public class UserTest {
 	}
 
 	@Test
+	public void testUserStringStringNull1() {
+		User u = new User(null,"asvas");
+		assertEquals("User",u.getUsername());		
+	}
+
+	@Test
+	public void testUserStringStringNull2() {
+		User u = new User("Name",null);
+		assertEquals("no@email.com",u.geteMail());		
+	}
+
+	@Test
+	public void testUserStringStringEmpty1() {
+		User u = new User("","address@email.com");
+		assertEquals("User",u.getUsername());		
+	}
+
+	@Test
+	public void testUserStringStringEmpty2() {
+		User u = new User("Name","");
+		assertEquals("no@email.com",u.geteMail());		
+	}
+
+	@Test
+	public void testUserStringStringEmpty() {
+		User u = new User(null,"asvas");
+		assertEquals("User",u.getUsername());		
+	}
+
+	@Test
 	public void testToString() {
-	   User u = new User("Name","user@mail.at");
-	   assertEquals("username:Name - email:user@mail.at",u.toString());
+		User u = new User("Name","user@mail.at");
+		assertEquals("username:Name - email:user@mail.at",u.toString());
+	}
+
+	@Test
+	public void testToStringNull() {
+		User u = new User(null,null);
+		assertEquals("username:User - email:no@email.com",u.toString());
 	}
 
 }
