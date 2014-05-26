18c7c0b9c003a872e747aa122ad6651ae91fc548
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-05-26 09:09:45 +0200

    initial commit

---
+++ src/test/MasterTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class MasterTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testInitVaadinRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+}
